d52c0cd8ab8cc9a0a23b462bcb0beb9cc3bf05b3
Mohamed DevOps <86662184+Mohamed-DevOps@users.noreply.github.com>
2023-11-30 12:49:30 +0200

    refactor initState & navigate to hame

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Constants {
+  // Colors //
   static const Color primaryColor = Color(0xff100b20);
+
+  static const transitionDuration = Duration(milliseconds: 250);
 }
