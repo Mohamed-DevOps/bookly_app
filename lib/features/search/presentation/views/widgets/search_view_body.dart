9d22097e6f2c6e30f0a90bacc23f2fd65beb405b
Mohamed DevOps <86662184+Mohamed-DevOps@users.noreply.github.com>
2023-12-02 17:14:12 +0200

    create SearchView

---
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'SearchViewBody is working',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
