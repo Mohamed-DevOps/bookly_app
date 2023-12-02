c6fa0a49e47c521f146b3c3f2e58ec5a11ab4909
Mohamed DevOps <86662184+Mohamed-DevOps@users.noreply.github.com>
2023-12-02 17:46:14 +0200

    SearchView done

--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,14 +1,12 @@
 import 'package:bookly/features/home/presentation/views/widgets/books_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      // shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
       itemCount: 10,
       itemBuilder: (context, index) {
         return const Padding(
