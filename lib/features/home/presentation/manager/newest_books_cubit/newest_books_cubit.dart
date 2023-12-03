d6d5a4466ef8080574401ad1dabaebfc619a9c0b
Mohamed DevOps <86662184+Mohamed-DevOps@users.noreply.github.com>
2023-12-03 12:58:27 +0200

    finish newest books cubit

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoadingState());
+
+    final result = await homeRepo.fetchNewestBooks();
+
+    result.fold((failure) {
+      emit(NewestBooksFailureState(errMessage: failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccessState(books: books));
+    });
+  }
 }
